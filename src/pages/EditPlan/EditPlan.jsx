d7a78ef52dbb0808200495920a66b00349da4f13
PrangonChakraborty-1805085 <1805085@ugrad.cse.buet.ac.bd>
2023-09-17 01:56:24 +0600

    updated page refresh in vercel.json

--- src/pages/EditPlan/EditPlan.jsx
+++ src/pages/EditPlan/EditPlan.jsx
@@ -44,6 +44,7 @@ export default function EditPlan() {
     top: "50%",
     left: "50%",
     transform: "translate(-50%, -50%)",
+    
     width: 400,
     bgcolor: "background.paper",
     border: "2px solid #000",
