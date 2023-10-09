1c2e8469d0d414c3c3f320993850bb9aae9faca4
PrangonChakraborty-1805085 <1805085@ugrad.cse.buet.ac.bd>
2023-10-10 02:14:45 +0600

    updated restuarants show

--- src/components/Google/Restuarants.jsx
+++ src/components/Google/Restuarants.jsx
@@ -1,43 +1,66 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react";
 
-const FoodPlacesMap = (props) => {
+const FoodPlacesMap = ({ google, apiKey, event }) => {
   const [places, setPlaces] = useState([]);
   const [selectedPlace, setSelectedPlace] = useState(null);
 
-  //   const myCurrentPlace = props.currentPlace;
-  //   const myCurrentLat = myCurrentPlace.lat;
-  //   const myCurrentLng = myCurrentPlace.lng;
-
   useEffect(() => {
-    async function fetchData() {
-      try {
-        console.log("sending request to get restuarants ...........");
-        const response = await fetch(
-          `${import.meta.env.VITE_BACKEND_URL}/api/foodPlace/home`,
-          {
-            method: "POST",
-            body: JSON.stringify({
-              lat: props.event.lat,
-              lng: props.event.lng,
-            }),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        );
-        const jsonData = await response.json();
-        //   dispatch(setPlan(jsonData));
-        setPlaces(jsonData);
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      } finally {
-        //   setLoading(false);
+    if (!google) return;
+
+    const map = new google.maps.Map(
+      document.getElementById("map" + event.lat + event.lng),
+      {
+        zoom: 14,
+        center: { lat: event.lat, lng: event.lng }, // Default center
       }
-    }
-    fetchData();
-  }, []);
+    );
+
+    const service = new google.maps.places.PlacesService(map);
+
+    const request = {
+      location: {
+        lat: event.lat, // Default latitude
+        lng: event.lng, // Default longitude
+      },
+      radius: 1500,
+      type: "restaurant",
+    };
+
+    service.nearbySearch(request, (results, status) => {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        console.log("results for rest ", results);
+        setPlaces(results);
+        renderNearbySearchResults(map, results);
+      } else {
+        console.error("Error fetching places:", status);
+      }
+    });
+  }, [google]);
+
+  const renderNearbySearchResults = (map, places) => {
+    const bounds = new google.maps.LatLngBounds();
+
+    places.forEach((place) => {
+      const marker = new google.maps.Marker({
+        map,
+        position: {
+          lat: place.geometry.location.lat(),
+          lng: place.geometry.location.lng(),
+        },
+        title: place.name,
+      });
+
+      google.maps.event.addListener(marker, "click", () => {
+        handleMarkerClick(place);
+      });
+
+      bounds.extend(place.geometry.location);
+    });
+
+    map.fitBounds(bounds);
+  };
 
   const handleMarkerClick = (place) => {
     setSelectedPlace(place);
@@ -51,75 +74,29 @@ const FoodPlacesMap = (props) => {
     setSelectedPlace(null);
   };
 
-  //! this is the custom marker icon for map showing
-  const redIconSize = new props.google.maps.Size(40, 40); // Smaller size
-  const customRedMarkerIcon = {
-    url: "https://maps.google.com/mapfiles/ms/icons/red-dot.png", // Red marker icon
-    scaledSize: redIconSize,
-  };
-  const blueIconSize = new props.google.maps.Size(60, 60); // Larger size
-  const customBlueMarkerIcon = {
-    url: "https://maps.google.com/mapfiles/ms/icons/blue-dot.png", // Blue marker icon
-    scaledSize: blueIconSize,
-  };
-
   return (
     <div className="py-8 pt-5 flex flex-col items-center justify-center md:flex-nowrap rounded-lg shadow-lg mb-4 w-[50rem]">
       <div className="flex flex-col items-center justify-evenly min-h-full min-w-full  p-4">
         <h1 className="text-xl font-semibold mb-4 min-w-full">
           Food Places Near You
         </h1>
-        <div className="min-w-full h-64 relative ">
-          <Map
-            google={props.google}
-            zoom={14}
-            style={{ width: "100%", height: "100%" }}
-            initialCenter={{
-              // lat: 23.746466,
-              // lng: 90.376015,
-              lat: props.event.lat,
-              lng: props.event.lng,
-            }}
-          >
-            <Marker
-              key={2}
-              position={{
-                // lat: 23.746466,
-                // lng: 90.376015,
-                lat: props.event.lat,
-                lng: props.event.lng,
-              }}
-              icon={customBlueMarkerIcon} // Use the custom marker icon
-              //   onMouseover={() => handleMarkerMouseover(place)}
-              //   onMouseout={handleMarkerMouseout}
-            />
-            {places.map((place) => (
-              <Marker
-                key={place.place_id}
-                position={{
-                  lat: place.geometry.location.lat,
-                  lng: place.geometry.location.lng,
-                }}
-                icon={customRedMarkerIcon} // Use the custom marker icon
-                onClick={() => handleMarkerClick(place)}
-                onMouseover={() => handleMarkerMouseover(place)}
-                onMouseout={handleMarkerMouseout}
-              />
-            ))}
-            {selectedPlace && (
-              <InfoWindow
-                marker={selectedPlace}
-                visible={true}
-                onClose={handleMarkerMouseout}
-              >
-                <div className="p-2">
-                  <h2>{selectedPlace.name}</h2>
-                  <p>{selectedPlace.vicinity}</p>
-                  {/* Add more details here */}
-                </div>
-              </InfoWindow>
-            )}
-          </Map>
+        <div
+          className="min-w-full h-64 relative "
+          id={"map" + event.lat + event.lng}
+        >
+          {selectedPlace && (
+            <InfoWindow
+              marker={selectedPlace}
+              visible={true}
+              onClose={handleMarkerMouseout}
+            >
+              <div className="p-2">
+                <h2>{selectedPlace.name}</h2>
+                <p>{selectedPlace.vicinity}</p>
+                {/* Add more details here */}
+              </div>
+            </InfoWindow>
+          )}
         </div>
       </div>
     </div>
